45eaf0a675659f843be0c85e1e4af263b0532ef5
Andrzej <andrzejrusinowski@gmail.com>
2024-01-06 20:14:00 +0100

    added recoursive functionality

--- JsonReplacer.js
+++ JsonReplacer.js
@@ -4,7 +4,62 @@ class JsonReplacer {
         this.targetId = targetId;
     }
 
+
+
+
     replace() {
+        fetch(this.jsonUrl)
+            .then(response => response.json())
+            .then(data => {
+
+
+                const outerElementId = this.targetId;
+                document.getElementById(outerElementId).innerHTML = cycleThrough(data, outerElementId);
+
+            })
+            .catch(error => console.error(error));
+
+
+
+
+        function cycleThrough(data, outerElementId) {
+
+            const targetElement = document.getElementById(outerElementId);
+            let targetContent = targetElement.innerHTML;
+
+
+            Object.keys(data).forEach(key => {
+
+
+                if (Array.isArray(data[key])) {
+                    let newContent = '';
+                    for (let element of data[key]) {
+                        newContent += cycleThrough(element, key)
+                    }
+                    const targetElement = document.getElementById(key);
+                    let subtargetContent = targetElement.innerHTML;
+                    let SubPattern = new RegExp(`${subtargetContent}`, 'g');
+                    targetContent = targetContent.replace(SubPattern, newContent);
+
+                };
+
+                const pattern = new RegExp(`{{${key}}}`, 'g');
+                targetContent = targetContent.replace(pattern, data[key]);
+
+
+
+            });
+            return targetContent;
+        }
+    }
+
+
+
+
+
+
+
+    replaceOLD() {                                                    //old function - non recursive
         fetch(this.jsonUrl)
             .then(response => response.json())
             .then(data => {
@@ -37,4 +92,5 @@ class JsonReplacer {
             })
             .catch(error => console.error(error));
     }
+
 }
--- README.md
+++ README.md
@@ -7,7 +7,10 @@ The script takes two parameters: the URL of the JSON file and the ID of the HTML
 
 The script first retrieves the JSON file from the indicated URL using the fetch interface. It then searches for the HTML element with the indicated ID and retrieves its innerHTML property.
 
-The script then uses JSON.parse to parse the JSON file into a JavaScript object. The object is then searched recursively, and for each key-value pair, the corresponding value is checked. If the value is a string, the script searches the template for occurrences of the key enclosed in double curly brackets and converts them into a value. If the value is. an array, the script checks if there is a corresponding HTML element with an ID that matches the key. If it exists, the HTML element is used as a template to create new HTML elements for each element in the array.
+V2 Update
+The script works recursively and you can apply more cavities using arrays and elements that have IDs.
+
+The script then uses JSON.parse to parse the JSON file into a JavaScript object. 
 
 Finally, the script replaces the innerHTML of the original HTML element with the modified template, and the page is updated with the new HTML.
 
